0daa83ed15f675bafa9609e7eec9299644327ca6
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-09-11 18:12:04 +0100

    make it export and added some sanity checkers

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const axios = require('axios'); 
-const cheerio = require('cheerio'); 
+const cheerio = require('cheerio');
+const fs = require('fs')
 
-axios.get('http://ennaharonline.com/algeria/')
-.then(({data}) => {
-    const $ = cheerio.load(data);
-    const newsDZ = $('.card__cntn')
-    .map((_,news) =>{
-        const $news = $(news);
-        const newsHead = $news.find('.bunh').text()
-        const newsDes = $news.find('.card__desc').text()
-        return {'news':newsHead, 'description':newsDes}
+const scrapedData = [];
+const urls = ['https://www.ennaharonline.com/algeria/', 'https://www.ennaharonline.com/algeria/page/2/', 'https://www.ennaharonline.com/algeria/page/3/']
+async function scrape() {
+    for (const url of urls){
+        const response = await axios.get(url)
+        if (response.status == 200){
+            console.log(`scraping : ${url}`)
+            const html = response.data
+            const $ = cheerio.load(html)
+            const totalNews = $('.card__meta')
+            for (const news of totalNews ){
+                const header = $(news).find('.bunh').text()
+                const date = $(news).find('.card__mfit').text()
+                scrapedData.push({
+                    'title':header,
+                    'date':date
+                })
+            }
+        }
+    }
+    fs.writeFile('EnnaharData', JSON.stringify(scrapedData),(err) =>{
+        if (err) throw err;
+        console.log('File saved')
     })
-    .toArray()
-    console.log(newsDZ)
-}).catch((err) => console.log(err))
+}
+scrape();
