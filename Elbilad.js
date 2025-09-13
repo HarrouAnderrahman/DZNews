efaf12623e6a3dcbbcead031c2c1742a33e1c7e1
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-09-13 17:41:35 +0100

    made it more professional

--- Elbilad.js
+++ Elbilad.js
@@ -1,3 +1,5 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
 const fs = require('fs')
+
+const scrapedData = [];
--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const { error } = require('console');
 const fs = require('fs')
 
 const scrapedData = [];
-const pages = 1; // number of pages i want to scrape
 const urls = []
-for (let i = 1;i<=pages;i++){
+async function scrape(pages) {
+    for (let i = 1;i<=pages;i++){
     let page = `https://www.ennaharonline.com/algeria/page/${i}/`
     urls.push(page)
-}
-async function scrape() {
+    }
     for (const url of urls){
         const response = await axios.get(url)
         if (response.status == 200){
@@ -18,6 +17,7 @@ async function scrape() {
             const html = response.data
             const $ = cheerio.load(html)
             const totalNews = $('.card__meta')
+            let n = 0
             for (const news of totalNews ){
                 const header = $(news).find('.bunh').text()
                 const date = $(news).find('.card__mfit').attr('datetime')
@@ -27,15 +27,17 @@ async function scrape() {
                 const author = ($1('.sgb1__amta').find('[href="#"]').text()).replace("\n\t\t\t\t\t\t\t\t\t\t\t", "")
                 const content = $1('.artx').find('p').text()
                 if(date){
-                scrapedData.push({
-                    'title':header,
-                    'date':date,
-                    'link':link,
-                    'content':{
-                        'author':author,
-                        'content':content
-                    }
-                })
+                    n = n + 1;
+                    console.log(`Scraping article ${n}...`)
+                    scrapedData.push({
+                        'title':header,
+                        'date':date,
+                        'link':link,
+                        'content':{
+                            'author':author,
+                            'content':content
+                        }
+                    })
                 }
             }
         }
@@ -45,4 +47,4 @@ async function scrape() {
         console.log('File saved')
     })
 }
-scrape();
+scrape(1);
