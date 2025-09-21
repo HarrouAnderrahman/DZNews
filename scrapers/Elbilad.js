b2ebf01b5499e92304ef03376d50a49922c9fc50
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-09-21 15:40:56 +0100

    fixed elbilad

--- scrapers/Elbilad.js
+++ scrapers/Elbilad.js
@@ -2,26 +2,23 @@ const { error } = require('console');
 const fs = require('fs')
 const puppeteer = require('puppeteer')
 
-const scrapedData = [];
-
 async function run() {
     const browser = await puppeteer.launch();
     console.log('Opening Browser ...')
     const page = await browser.newPage();
-    await page.goto('https://www.elbilad.net/national', {timeout:60000})
+    await page.goto('https://www.elbilad.net/national')
     console.log('Accessing Elbilad ...')
     await page.waitForSelector('#categoryArticles > ul > li');
     console.log('Extracting News ...')
-    await page.$$eval('#categoryArticles > ul > li', news => news.map(
+    let scrapedData = await page.$$eval('#categoryArticles > ul > li', news => news.map(
         (articl) =>{ 
-             const title = articl.querySelector('h3').textContent.trim()
-             scrapedData.push({
-                'title': title
-             })
+             const title = articl.querySelector('h3')?.textContent.trim() || ''
+            return {title}
             }
     ))
     await browser.close();
-    fs.writeFile('ElbiladData.JSON', JSON.stringify(scrapedData),(err)=>{
+    const filteredData = scrapedData.filter(item => item.title); // to remove null values
+    fs.writeFile('ElbiladData.JSON', JSON.stringify(filteredData),(err)=>{
         if (err) throw error
         console.log('File Saved!')
     })
