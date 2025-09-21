ad64ea826f9de954c6971f3c865b9223c97a6e01
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-09-21 16:11:23 +0100

    Added the link to the scraped data and made the scraping faster

--- scrapers/Elbilad.js
+++ scrapers/Elbilad.js
@@ -6,14 +6,24 @@ async function run() {
     const browser = await puppeteer.launch();
     console.log('Opening Browser ...')
     const page = await browser.newPage();
+    await page.setRequestInterception(true);// make it skip loading the unnecessary things to make the loading faster
+    page.on('request', (req) => {
+        if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
+            req.abort();
+        } 
+        else {
+            req.continue();
+        }
+    });
     await page.goto('https://www.elbilad.net/national')
     console.log('Accessing Elbilad ...')
     await page.waitForSelector('#categoryArticles > ul > li');
     console.log('Extracting News ...')
     let scrapedData = await page.$$eval('#categoryArticles > ul > li', news => news.map(
         (articl) =>{ 
-             const title = articl.querySelector('h3')?.textContent.trim() || ''
-            return {title}
+            const title = articl.querySelector('h3')?.textContent.trim() || ''
+            const link = articl.querySelector('h3 > a')?.href || ''
+            return {title, link}
             }
     ))
     await browser.close();
