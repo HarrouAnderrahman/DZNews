20c8445f6939a060a5a852af4ee96c67b633e647
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-09-23 13:06:12 +0100

    Added error handling

--- scrapers/Elbilad.js
+++ scrapers/Elbilad.js
@@ -3,34 +3,42 @@ const fs = require('fs')
 const puppeteer = require('puppeteer')
 
 async function run() {
-    const browser = await puppeteer.launch();
-    console.log('Opening Browser ...')
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);// make it skip loading the unnecessary things to make the loading faster
-    page.on('request', (req) => {
-        if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
-            req.abort();
-        } 
-        else {
-            req.continue();
-        }
-    });
-    await page.goto('https://www.elbilad.net/national')
-    console.log('Accessing Elbilad ...')
-    await page.waitForSelector('#categoryArticles > ul > li');
-    console.log('Extracting News ...')
-    let scrapedData = await page.$$eval('#categoryArticles > ul > li', news => news.map(
-        (articl) =>{ 
-            const title = articl.querySelector('h3')?.textContent.trim() || ''
-            const link = articl.querySelector('h3 > a')?.href || ''
-            return {title, link}
+    let browser;
+    try {
+        browser = await puppeteer.launch();
+        console.log('Opening Browser ...')
+        const page = await browser.newPage();
+        await page.setRequestInterception(true);// make it skip loading the unnecessary things to make the loading faster
+        page.on('request', (req) => {
+            if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
+                req.abort();
+            } 
+            else {
+                req.continue();
             }
-    ))
-    await browser.close();
-    const filteredData = scrapedData.filter(item => item.title); // to remove null values
-    fs.writeFile('ElbiladData.json', JSON.stringify(filteredData),(err)=>{
-        if (err) throw error
+        });
+        await page.goto('https://www.elbilad.net/national')
+        console.log('Accessing Elbilad ...')
+        await page.waitForSelector('#categoryArticles > ul > li');
+        console.log('Extracting News ...')
+        let scrapedData = await page.$$eval('#categoryArticles > ul > li', news => news.map(
+            (articl) =>{ 
+                const title = articl.querySelector('h3')?.textContent.trim() || ''
+                const link = articl.querySelector('h3 > a')?.href || ''
+                return {title, link}
+                }
+        ))
+        const filteredData = scrapedData.filter(item => item.title); // to remove null values
+        fs.writeFile('ElbiladData.json', JSON.stringify(filteredData),(err)=>{
+        if (err) throw err
         console.log('File Saved!')
     })
+    }
+    catch (error){
+        console.error(error)
+    }
+    finally{
+        await browser.close();
+    }
 }
 run();
