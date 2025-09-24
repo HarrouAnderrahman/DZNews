e6cdd435285ab745a9ac4fb091921b12952b83d2
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-09-24 17:38:23 +0100

    Having a problem

--- scrapers/Elbilad.js
+++ scrapers/Elbilad.js
@@ -1,12 +1,16 @@
-const { error } = require('console');
 const fs = require('fs')
 const puppeteer = require('puppeteer')
 const axios = require('axios')
 const cheerio = require ('cheerio')
 
-
-
-async function run() {
+// I need to make it so the user chooses a date and it will scrape until the date is due
+const userDate = new Date('2025-09-15')
+// first lemme just make a function that transform the articl date to a valid js date
+async function transformDate(str) {
+    let [stopDay, stopMonth, stopYear] = str.split('-')
+    return new Date(`${stopYear}-${stopMonth}-${stopDay}`)
+}
+async function run(choosenDate) {
     let browser;
     let scrapedData = [];
     try {
@@ -25,45 +29,72 @@ async function run() {
         await page.goto('https://www.elbilad.net/national')
         console.log('Accessing Elbilad ...')
         await page.waitForSelector('#categoryArticles > ul > li');
-        console.log('Extracting News ...')
-        let scraped = await page.$$eval('#categoryArticles > ul > li', news => news.map(
-        (articl) =>{ 
-                const title = articl.querySelector('h3')?.textContent.trim() || ''
-                const link = articl.querySelector('h3 > a')?.href || ''
-                const date = articl.querySelector('ul > li:nth-child(3)')?.textContent.trim() || ''
-                return ({title,link,date})
-            }
-        ))
-        for (let articl of scraped ){
-            if (articl.link){
-                        const response = await axios.get(articl.link)
-                        if (response.status == 200){
-                            console.log('scraping context...')
-                            const html = await response.data
-                            const $ = cheerio.load(html)
-                            const contentData = $('.cols-a').find('p').text().replace("{{ key }}: {{ error[0] }}\n        بريدك الالكتروني\n        \n        اشتراك\n    10922 V 27500 7/8Satellite : Nilesat 7.0 ° West", "")
-                            const author = $('.title').find('.strong')?.text() || 'no author'
-                            scrapedData.push({
-                                'title': articl.title,
-                                'link': articl.link,
-                                'date': articl.date,
-                                'content': {
-                                    'contentData':contentData,
-                                    'author':author
-                                }
-                            })
-                        }
-                        else {
-                            throw new Error("Failed scraping the context")
-                        }
+        console.log('Checking News ...')
+        // makign a while loop to extract only news that's after the user's choosen date
+        let keepGoing = true
+        while (keepGoing){
+            let articlCount = await page.$$eval(
+                '#categoryArticles > ul > li',
+                 articleNum => articleNum.length - 1)
+            let extractLastDate = await page.$eval(
+                `#categoryArticles > ul > li:nth-child(${articlCount}) > ul > li:nth-child(3)`,
+                 date => date.textContent.trim())
+            let stopDate = await transformDate(extractLastDate);
+            console.log(stopDate)
+            if (choosenDate < stopDate){
+                await page.waitForSelector("#categoryArticles > ul > li.link-btn")
+                await page.click("#categoryArticles > ul > li.link-btn")
+                console.log('clicked the load more button...')
+                await page.waitForFunction(
+                    prevCount => {
+                        document.querySelector("#categoryArticles > ul > li").length - 1 > prevCount
+                    },
+                    {}, // no options
+                    articlCount // <<--- this is passed as prevCount
+                )
+                articlCount += 10
             } else {
-                continue;
+                keepGoing = false
+                let scraped = await page.$$eval('#categoryArticles > ul > li', news => news.map(
+                (articl) =>{ 
+                    const title = articl.querySelector('h3')?.textContent.trim() || ''
+                    const link = articl.querySelector('h3 > a')?.href || ''
+                    const date = articl.querySelector('ul > li:nth-child(3)')?.textContent.trim() || ''
+                    return ({title,link,date})
+                }
+            ))
+            for (let articl of scraped ){
+                if (articl.link){
+                            const response = await axios.get(articl.link)
+                            if (response.status == 200){
+                                console.log('scraping content...')
+                                const html = await response.data
+                                const $ = cheerio.load(html)
+                                const contentData = $('.cols-a').find('p').text().replace("{{ key }}: {{ error[0] }}\n        بريدك الالكتروني\n        \n        اشتراك\n    10922 V 27500 7/8Satellite : Nilesat 7.0 ° West", "")
+                                const author = $('.title').find('.strong')?.text() || 'no author'
+                                scrapedData.push({
+                                    'title': articl.title,
+                                    'link': articl.link,
+                                    'date': articl.date,
+                                    'content': {
+                                        'contentData':contentData,
+                                        'author':author
+                                    }
+                                })
+                            }
+                            else {
+                                throw new Error("Failed scraping the context")
+                            }
+                } else {
+                    continue;
+                }
+            }
             }
         }
-        await fs.writeFile('ElbiladData.json', JSON.stringify(scrapedData),(err)=>{
-        if (err) throw err
-        console.log('File Saved!')
-    })
+            await fs.writeFile('ElbiladData.json', JSON.stringify(scrapedData),(err)=>{
+            if (err) throw err
+            console.log('File Saved!')
+        })
     }
     catch (error){
         console.error(error)
@@ -72,4 +103,4 @@ async function run() {
         await browser.close();
     }
 }
-run();
+run(userDate);
