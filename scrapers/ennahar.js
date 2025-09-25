edf807088dbce36afaf979ed3180c4b4cd99876a
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-09-25 14:22:40 +0100

    Fixed the ennahar scraper, added error handling , and made the user choose the end date (changed the logic)

--- scrapers/ennahar.js
+++ scrapers/ennahar.js
@@ -4,47 +4,62 @@ const { error } = require('console');
 const fs = require('fs')
 
 const scrapedData = [];
-const urls = []
-async function scrape(pages) {
-    for (let i = 1;i<=pages;i++){
-    let page = `https://www.ennaharonline.com/algeria/page/${i}/`
-    urls.push(page)
-    }
-    for (const url of urls){
-        const response = await axios.get(url)
-        if (response.status == 200){
-            console.log(`scraping : ${url}`)
-            const html = response.data
-            const $ = cheerio.load(html)
-            const totalNews = $('.card__meta')
-            let n = 0
-            for (const news of totalNews ){
-                const header = $(news).find('.bunh').text()
-                const date = $(news).find('.card__mfit').attr('datetime')
-                const link = $(news).find('.bunh').attr('href') // lets scrape this page too
-                const pageContent = await axios.get(link)
-                const $1 = cheerio.load(pageContent.data)
-                const author = ($1('.sgb1__amta').find('[href="#"]').text()).replace("\n\t\t\t\t\t\t\t\t\t\t\t", "")
-                const content = $1('.artx').find('p').text()
-                if(date){
-                    n = n + 1;
-                    console.log(`Scraping article ${n}...`)
-                    scrapedData.push({
-                        'title':header,
-                        'date':date,
-                        'link':link,
-                        'content':{
-                            'author':author,
-                            'content':content
+const userDate = new Date('2025-09-25')
+async function scrape(choosenDate) {
+    try {
+        let keepGoing = true
+        let i = 1
+        while(keepGoing){
+            let page = `https://www.ennaharonline.com/algeria/page/${i}/`
+            const response = await axios.get(page)
+            if (response.status == 200){
+                console.log(`scraping : ${page}`)
+                const html = response.data
+                const $ = cheerio.load(html)
+                const totalNews = $('.card__meta')
+                let n = 0
+                for (const news of totalNews ){
+                    const header = $(news).find('.bunh').text()
+                    const date = $(news).find('.card__mfit').attr('datetime')
+                    const link = $(news).find('.bunh').attr('href') // lets scrape this page too
+                    const pageContent = await axios.get(link)
+                    const $1 = cheerio.load(pageContent.data)
+                    const author = ($1('.sgb1__amta').find('[href="#"]').text()).replace("\n\t\t\t\t\t\t\t\t\t\t\t", "")
+                    const content = $1('.artx').find('p').text()
+                    if(date){
+                        dateObj = new Date (date)
+                        if(choosenDate < dateObj){
+                            n += 1;
+                            console.log(`Scraping article ${n}...`)
+                            scrapedData.push({
+                                'title':header,
+                                'date':date,
+                                'link':link,
+                                'content':{
+                                    'author':author,
+                                    'content':content
+                                }
+                            })
+                        }else{
+                            keepGoing = false
+                            console.log("Reached the end date")
+                            break;
                         }
-                    })
+                    }
                 }
+                if(keepGoing) i++;
+            } else{
+                keepGoing = false
+                throw new Error("Coudln't load the Page , response.status != 200")
             }
+
         }
+    } catch (error) {
+        console.error(error)
     }
     fs.writeFile('EnnaharData.json', JSON.stringify(scrapedData),(err) =>{
         if (err) throw err;
         console.log('File saved')
     })
 }
-scrape(1);
+scrape(userDate);
