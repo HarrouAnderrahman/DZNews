995f43dd6e272b0156ad247a8808816d6779ff5f
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-10-02 17:54:32 +0100

    0.5.3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const program = new Command(); // <-- setting up commander
 program
   .name(chalk.bold.bgCyan('DZNews-CLI'))
   .description(chalk.bold('Algerian News Webscraper'))
-  .version('0.5.1');
+  .version('0.5.3');
 
 
 program // listing the sources
