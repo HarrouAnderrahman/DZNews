ad28d7bd29ac44a5f557375e9b86b27ba4356c45
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-10-12 18:57:18 +0100

    Better dates errors handling

--- scrapers/utils/dateHandling.js
+++ scrapers/utils/dateHandling.js
@@ -1,13 +1,25 @@
-
-async function dateToString(choosenDate) { // could be used in saving (like naming the saved data from the choosen date from this date) 
-    let dateStr = `${choosenDate.getFullYear()}-${choosenDate.getMonth() + 1}-${choosenDate.getDate()}`
-    return dateStr
+async function dateToString(choosenDate) {
+  // could be used in saving (like naming the saved data from the choosen date from this date)
+  const zero = "0";
+  let dateStr = `${choosenDate.getFullYear()}-${
+    choosenDate.getMonth() + 1
+  }-${choosenDate.getDate()}`;
+  if (choosenDate.getMonth() < 9) { // i hated how it didn't display a zero before a single number (i might be stupid)
+    let arr = dateStr.split("");
+    arr.splice(5, 0, ...zero);
+    dateStr = arr.join("");
+  }
+  if (choosenDate.getDate() < 10) {
+    let arr = dateStr.split("");
+    arr.splice(8, 0, ...zero);
+    dateStr = arr.join("");
+  }
+  return dateStr;
 }
 
 async function transformDate(str) {
-    let [stopDay, stopMonth, stopYear] = str.split('-')
-    return new Date(`${stopYear}-${stopMonth}-${stopDay}`)// need it so it transforms elbilad invalid date to a valid one
+  let [stopDay, stopMonth, stopYear] = str.split("-");
+  return new Date(`${stopYear}-${stopMonth}-${stopDay}`); // need it so it transforms elbilad invalid date to a valid one
 }
 
-
-module.exports = {dateToString, transformDate}
+module.exports = { dateToString, transformDate };
