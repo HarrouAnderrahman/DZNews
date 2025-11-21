b41a7c2a0f22cc7b01742a36c3a4a3293bf91484
Harrou Abderrahman <harrouabdelrrahmane@gmail.com>
2025-11-21 18:43:31 +0100

    Fixed a typo on the UA

--- scrapers/utils/axiosConfig.js
+++ scrapers/utils/axiosConfig.js
@@ -2,17 +2,17 @@ const axios = require("axios");
 
 const userAgents = [
   {
-    ua: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ... Chrome/124.0.0.0 Safari/537.36",
+    ua: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     platform: '"Windows"',
     secChUa: '"Chromium";v="124", "Not-A.Brand";v="99"',
   },
   {
-    ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) ... Safari/605.1.15",
+    ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
     platform: '"macOS"',
     secChUa: '"Safari";v="17", "Not-A.Brand";v="99"',
   },
   {
-    ua: "Mozilla/5.0 (X11; Linux x86_64) ... Chrome/123.0.0.0 Safari/537.36",
+    ua: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
     platform: '"Linux"',
     secChUa: '"Chromium";v="123", "Not-A.Brand";v="99"',
   },
